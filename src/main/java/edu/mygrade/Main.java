b8c209ddc73068c2ca52b07cf9361b727bcddc54
victor-jo <victor-jo@github.com>
2022-02-07 22:41:22 +0900

    #add : 예시 입력 및 Main 함수 추가

---
+++ src/main/java/edu/mygrade/Main.java
@@ -0,0 +1,53 @@
+package edu.mygrade;
+
+import edu.mygrade.answer.Answer;
+import edu.mygrade.answer.AnswerCollector;
+import edu.mygrade.score.Score;
+import edu.mygrade.score.ScoreCollector;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int maxProblemCount = Integer.parseInt(br.readLine());
+
+		// 제출 정답
+		AnswerCollector inputAnswers = new AnswerCollector(maxProblemCount);
+		{
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int index = maxProblemCount;
+			while (index-- > 0) {
+				inputAnswers.add(new Answer(Integer.parseInt(st.nextToken())));
+			}
+		}
+
+		// 답안지
+		AnswerCollector answers = new AnswerCollector(maxProblemCount);
+		{
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int index = maxProblemCount;
+			while (index-- > 0) {
+				answers.add(new Answer(Integer.parseInt(st.nextToken())));
+			}
+		}
+
+		// 총 점수
+		final double totalScore = 100;
+		ScoreCollector scoreCollector = new ScoreCollector(new Score(totalScore));
+
+		scoreCollector.setMaxProblemCount(maxProblemCount);
+
+		int incorrectCount = inputAnswers.equalsAndGetIncorrectCount(answers);
+		double expectScore = totalScore - (scoreCollector.getAmountScore().getScore() * incorrectCount);
+
+		System.out.println("expectScore = " + expectScore);
+		System.out.println("incorrectCount = " + incorrectCount);
+	}
+
+}
